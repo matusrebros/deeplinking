35d8ecdbc8ae6144f6687306a410546a6d76d614
matusrebros <rebros.matus@gmail.com>
2021-06-23 11:25:55 +0200

    deep link creator done

---
+++ short/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Deeplinking</title>
+</head>
+
+<body>
+
+	<form method="POST" action="deep.php">
+		<input type="text" name="IgUsername" placeholder="IG username" />
+		<input type="submit" value="Deep It" />
+	</form>
+
+</body>
+
+</html>
